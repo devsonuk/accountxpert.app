e6cc0b31e784540b2fac7dec917451f1217f715d
Sonu Kumar <devsonuk02@gmail.com>
2025-02-16 17:46:50 +0530

    Add biome config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,43 @@
-import { defineConfig } from '@rsbuild/core';
+import { defineConfig, loadEnv } from '@rsbuild/core';
+import { pluginBasicSsl } from '@rsbuild/plugin-basic-ssl';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from '@rsbuild/plugin-sass';
+import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { pluginTypeCheck } from '@rsbuild/plugin-type-check';
+
+const { publicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  source: {
+    define: publicVars,
+    transformImport: [
+      {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+      },
+    ],
+  },
+  plugins: [
+    pluginBasicSsl(),
+    pluginReact(),
+    pluginTypeCheck(),
+    pluginSvgr({
+      svgrOptions: {
+        exportType: 'named',
+        typescript: true,
+      },
+    }),
+    pluginSass(),
+  ],
+  output: {
+    cleanDistPath: true,
+    sourceMap: {
+      js:
+        process.env.NODE_ENV === 'production'
+          ? 'source-map'
+          : 'cheap-module-source-map',
+      css: process.env.NODE_ENV !== 'production',
+    },
+    polyfill: 'usage',
+  },
 });
